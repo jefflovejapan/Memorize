302f7d0245200a547800cefd50fa0837b2f44aa3
Jeffrey Blagdon <jeffblagdon@gmail.com>
2020-08-03 11:05:26 -0300

    Initial commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Jeffrey Blagdon on 2020-08-03.
+//  Copyright © 2020 polyergy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
